81a3837a58a0e049167b85bf044fbe128dc192f1
Erik Samuelsson <erik.samuelsson@gmail.com>
2018-03-16 00:17:31 +0100

    Add login function

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,11 +1,21 @@
 import jwt from 'jsonwebtoken';
-import httpStatus from 'http-status';
 import User from '../models/user.model';
 
 const authController = {};
 
 authController.login = (req, res) => {
-    console.log('hej');
+    User.findOne({ username: req.body.username }, (err, user) => {
+        if (err) return res.status(500).json({ message: 'We fucked up!' });
+        if (!user) return res.status(401).json({ message: 'User not found.' });
+
+        if (!user.validPassword(req.body.password)) {
+            return res.status(401).json({ message: 'Wrong password.' });
+        }
+
+        return res.json({
+            token: jwt.sign({ username: user.username, id: user.id }, 'THIS_IS_GOING_IN_ENV_CONF_LATER')
+        });
+    });
 };
 
 export default authController;
