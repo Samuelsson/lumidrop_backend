a111d8daa729ae73dfa09a2e8eda2574af78c8c1
Erik Samuelsson <1119465+Samuelsson@users.noreply.github.com>
2018-04-14 23:11:47 +0200

    Add error handling when trying to login

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,22 +2,29 @@ import jwt from 'jsonwebtoken';
 import User from '../user/user.model';
 
 const authController = {};
+const privateKey = 'THIS_SECRET_IS_GOING_IN_ENV_CONF_LATER';
 
 authController.login = (req, res) => {
     User.findOne({ username: req.body.username }, (err, user) => {
-        if (err) return res.status(500).json({ message: 'We fucked up!' });
-        if (!user) return res.status(401).json({ message: 'User not found.' });
+        if (err) {
+            return res.status(500).json({ message: 'We fucked up!' });
+        } else if (user) {
+            user.validPassword(req.body.password).then((valid) => {
+                if (!valid) return res.status(401).json({ message: 'Invalid credentials.' });
 
-        user.validPassword(req.body.password).then((valid) => {
-            if (!valid) return res.status(401).json({ message: 'Wrong password.' });
-
-            return res.json({
-                token: jwt.sign({
+                const jwtBearerToken = jwt.sign({
                     username: user.username,
                     id: user.id
-                }, 'THIS_SECRET_IS_GOING_IN_ENV_CONF_LATER', { expiresIn: 86400 })
+                }, privateKey, { expiresIn: 120 });
+
+                return res.json({
+                    token: jwtBearerToken,
+                    message: 'Successfully logged in.'
+                });
             });
-        });
+        } else {
+            return res.status(401).json({ message: 'Invalid credentials.' });
+        }
     });
 };
 
